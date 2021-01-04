6aa8017355251ac236b2cdb345df2aba9b706f8a
matthewthechickenman <stead873@gmail.com>
2021-01-04 23:12:33 +0000

    🎨 Use the raw User instead of CacheUser

--- src/private_utils/collection.ts
+++ src/private_utils/collection.ts
@@ -1,15 +1,15 @@
-import {CacheUser, User} from "../structs";
+import {User} from "../structs";
 
 export class Collection {
-    raw: Array<CacheUser>
+    raw: Array<User>
     constructor() {
         this.raw = [];
     }
     set(user: User) {
-        this.raw.push({user: user})
+        this.raw.push(user)
         return this;
     }
     get(id: String) {
-        return this.raw.find((g) => g.user.id == id)
+        return this.raw.find((g) => g.id == id)
     }
 }
